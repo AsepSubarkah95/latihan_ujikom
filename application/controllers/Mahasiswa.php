3f8c0087a102eeea759c528f2abb11b287b340fe
AsepSubarkah95 <asjunjunior@gmail.com>
2021-04-26 21:56:27 +0700

    Menambahkan Bootstrap dan Menampilkan Data Mahasiswa

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -4,6 +4,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Mahasiswa extends CI_Controller {
 	public function index()
 	{
-        $this->load->view('mhs/index');
+		$this->load->model('Mahasiswa_model');
+
+		$data['mahasiswas'] = $this->Mahasiswa_model->get_last_five_entries();
+
+        $this->load->view('templates/header');
+		$this->load->view('mhs/index',$data);
+		$this->load->view('templates/footer');
 	}
 }
